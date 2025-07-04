67e64a6383bd7fed145362486bcd12ee0ec55af3
Techwolf78 <mrajaypawar207@gmail.com>
2025-07-04 17:22:15 +0530

    your message

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,19 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useLocation,
-} from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
-import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import ProtectedRoute from "./routes/ProtectedRoute";
-import DashboardLayout from "./components/DashboardLayout";
-import Dashboard from "./pages/Dashboard";
-import Admin from "./pages/Admin";
-import Sales from "./pages/Sales";
-import Placement from "./pages/Placement";
-import LearningDevelopment from "./pages/LearningDevelopment";
-import DigitalMarketing from "./pages/Marketing";
-import Footer from "./pages/footer";
-import UpdateProfile from "./components/UpdateProfile";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import ProtectedRoute from './routes/ProtectedRoute';
+import DashboardLayout from './components/DashboardLayout';
+import Dashboard from './pages/Dashboard';
+import Admin from './pages/Admin';
+import Sales from './pages/Sales';
+import Placement from './pages/Placement';
+import LearningDevelopment from './pages/LearningDevelopment';
+import DigitalMarketing from './pages/Marketing';
+import Footer from './pages/footer';
+import UpdateProfile from './components/UpdateProfile';
 
 const AppContent = () => {
   const location = useLocation();
@@ -38,27 +33,26 @@ const AppContent = () => {
             </ProtectedRoute>
           }
         >
-          <Route index element={<Dashboard />} /> {/* /dashboard */}
+          <Route index element={<Dashboard />} />  {/* /dashboard */}
           <Route path="profile" element={<UpdateProfile />} />
           <Route path="admin" element={<Admin />} />
           <Route path="sales" element={<Sales />} />
           <Route path="placement" element={<Placement />} />
-          <Route
-            path="learning-development"
-            element={<LearningDevelopment />}
-          />
+          <Route path="learning-development" element={<LearningDevelopment />} />
           <Route path="marketing" element={<DigitalMarketing />} />
         </Route>
       </Routes>
 
-      {location.pathname === "/" && <Footer />}
+
+
+      {location.pathname === '/' && <Footer />}
     </>
   );
 };
 
 const App = () => (
-  <AuthProvider>
-    <Router basename="/sync">
+  <AuthProvider >
+    <Router >
       <AppContent />
     </Router>
   </AuthProvider>
