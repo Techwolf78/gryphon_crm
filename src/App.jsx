a34ff9758aa0927fb9981e4d49e9d43cbb61bbda
Techwolf78 <mrajaypawar207@gmail.com>
2025-09-16 16:29:45 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -73,7 +73,7 @@ const AppContent = () => {
 const App = () => (
   <MsalProviderWrapper>
     <AuthProvider>
-      <Router basename="/sync">
+      <Router basename="/">
         <AppContent />
       </Router>
     </AuthProvider>
